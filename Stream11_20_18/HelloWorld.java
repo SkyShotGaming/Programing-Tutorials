bc38565a77f727295c4ce2eadccf6092fa785627
Albert <45322893+SkyShotGaming@users.noreply.github.com>
2018-11-24 21:20:22 -0500

    Add files via upload

---
+++ Stream11_20_18/HelloWorld.java
@@ -0,0 +1,12 @@
+package Stream11_20_18;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+
+	}
+
+}
---
+++ Stream11_20_18/Math.java
@@ -0,0 +1,24 @@
+package Stream11_20_18;
+
+public class Math {
+	
+	static int Num2 = 19;
+	static int Num1 = 34;
+
+	public static void main(String[] args) {
+		
+		
+		int A = Num1 + Num2;
+		int M = Num1 * Num2;
+		int S = Num1 - Num2;
+		int D = Num1 / Num2;
+		
+		System.out.println(A); //Add
+		System.out.println(M);//Multiply
+		System.out.println(S);//Subtract
+		System.out.println(D); //Devision (No Remainder)
+		
+		
+	}
+
+}
